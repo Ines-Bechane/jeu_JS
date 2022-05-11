762542dafb99c7c79b13368495cba10dec63b357
Ines <iness.bechane@gmail.com>
2022-05-11 15:30:38 +0200

    js

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 alert("vous etes bien sur notre site")
 
+//LA PARTIE DU DOM: POUR MODIFIER LA PARTIE HTML LORS D EVENEMENTS
+//on a appris a modifier une div
+// document.getElementById'"madiv") : qui va recuperer l'emplacement de la div
+//innertHTML="le nouveau text"; qui modifier le HTML de la div
 document.getElementById("madiv").innerHTML="le nouveau text"
+
+/* ALGORITHMIQUE
+
+   1 VARIABLE
+   Lorsque on va stocke
+   ou manipulé on va avoir besoin de données on va mettre dans une variable 
+
+   2 CONDITIONS
+
+   3 BOUCLE
+
+   4 FONCTION
+
+*/
+//VARIABLE NUMERIQUE
+//CREATION DE LA VARIABLE NUMERIQUE AGE CONTENANT 34        
+age=30;
+alert(age);
+
+//VARIABLE CHAINE DE CARACTERE
+// CREATION DE LA VARIABLE CHAINE DE CARACTERE NOM CONTENANT BECHANE
+nom="melki";
+alert(nom)
