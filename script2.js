bf1af928ccf876ac08f543f6c3c782f29b63e6cd
Ines <iness.bechane@gmail.com>
2022-05-12 11:42:24 +0200

    js

--- script2.js
+++ script2.js
@@ -1,5 +1,15 @@
 
 /* Les evenements */
-function toto(){
+/*function toto(){
     alert("Vous avez cliqué sur le bouton")
+ }*/
+
+ function toto(){
+    // le prompt permet de demander à l'utilisateur via une boite de dialogue d'entrer un nombre
+    // on stocke l'information dans la variable nb_user
+   nb_user=prompt("devinez un nombre entre 1 et 100");
+   // On l'affiche
+   /*alert(nb_user);*/
+   console.log(nb_user);
+   location.assign("http://google.com")
 }
